027bc9bd5307bed3e2ce404f6280f641d7510dad
seventhmoon <funglam@7thmoon.com>
2016-04-26 13:24:27 +0800

    added - cloud endpoint added - new intent w/ extra added - asynctask loading data from cloud

--- app/src/main/java/com/androidfung/builditbigger/MainActivity.java
+++ app/src/main/java/com/androidfung/builditbigger/MainActivity.java
@@ -1,48 +1,44 @@
 package com.androidfung.builditbigger;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
 
 import com.example.fung.myapplication.backend.myApi.MyApi;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
-import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
-import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private TextView mTextViewJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        mTextViewJoke = (TextView) findViewById(R.id.textview_joke);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
+        loadJoke();
+
+
+
+    }
+    private void loadJoke(){
         Pair<Context, String> pair = new Pair<>(getApplicationContext(), "");
-
         new EndpointsAsyncTask().execute(pair);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,11 +53,10 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -79,13 +74,14 @@ public class MainActivity extends AppCompatActivity {
                         // options for running against local devappserver
                         // - 10.0.2.2 is localhost's IP address in Android emulator
                         // - turn off compression when running against local devappserver
-                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                            @Override
-                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                                abstractGoogleClientRequest.setDisableGZipContent(true);
-                            }
-                        });
+//                        .setRootUrl("http://10.0.2.2:8080/_ah/api/")
+                        .setRootUrl("https://nd801-backend.appspot.com/_ah/api/");
+//                        .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
+//                            @Override
+//                            public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
+//                                abstractGoogleClientRequest.setDisableGZipContent(true);
+//                            }
+//                        });
                 // end options for devappserver
 
                 myApiService = builder.build();
@@ -95,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             String name = params[0].second;
 
             try {
-                return myApiService.getRandomJoke.execute().getData();
+                return myApiService.getRandomJoke().execute().getData();
             } catch (IOException e) {
                 return e.getMessage();
             }
@@ -103,7 +99,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String result) {
-            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+//            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+//            mTextViewJoke.setText(result);
+
+            Intent intent = new Intent(getApplicationContext(), JokeDisplayActivity.class);
+//            EditText editText = (EditText) findViewById(R.id.edit_message);
+//            String message = editText.getText().toString();
+            intent.putExtra("JOKE_MESSAGE", result);
+            startActivity(intent);
         }
     }
 }
